fb08389fd2f9a1340caccc26370a03edeb80080f
Boao Huang <bhgc9@mail.missouri.edu>
2019-04-05 22:33:31 -0500

    Final work of color navigator

---
+++ Colors/Colors/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Boao Huang on 4/5/19.
+//  Copyright © 2019 Boao Huang. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
